8455f050e0225be85beedd00ba430dac24f8931f
Basharameez <126752711+Basharameez@users.noreply.github.com>
2025-08-21 14:48:19 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Enable CORS (adapt allow_origins for production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["techinmystle.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
